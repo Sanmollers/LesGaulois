4870c481d3051935893172f655fbd25d38dcfa6a
Sanmollers <mollersjean0@gmail.com>
2024-02-25 02:23:14 +0100

    test2

--- git_auto_push.sh
+++ git_auto_push.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 git config user.name "Sanmollers"
 git config user.email mollersjean0@gmail.com
-echo "Connection as Sanmollers... \n"
-read -p "Entrez le message du commit entre \"\" puis la touche Entrer ou la touche Entrer pour l'envoyer en urgence." commit_message
+echo -e "Connection as Sanmollers... \n"
+read -p "Entrez le message du commit entre \"\" puis la touche Entrer ou la touche Entrer pour l'envoyer en urgence. " commit_message
 if [ $commit_message -eq "" ]
 then
  git add . | git commit -m "push en urgence à relire !" | git push
