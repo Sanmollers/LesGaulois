5f25c5843a27918fd8d9c9464feea7d0f4442da3
Sanmollers <mollersjean0@gmail.com>
2024-02-25 02:41:39 +0100

    push en urgence à relire !

--- git_auto_push.sh
+++ git_auto_push.sh
@@ -1,13 +1,14 @@
+# - * - ENCODAGE: UTF-8 - * -
 #! /bin/sh
 git config user.name "Sanmollers"
 git config user.email mollersjean0@gmail.com
 echo -e "Connection as Sanmollers... \n"
 read -p "Entrez le message du commit entre \"\" puis la touche Entrer ou la touche Entrer pour l'envoyer en urgence. " commit_message
-if [ $commit_message -eq "" ]
+if [ $commit_message = ""]
 then
  git add . | git commit -m "push en urgence à relire !" | git push
  echo "push en urgence à relire !"
 else
  git add . | git commit -m "$commit_message" | git push
- echo "Votre projet a bien ete \"push\" !"
+ echo "Votre projet a bien été \"push\" avec le commit suivant : \"$commit_message\" !"
 fi
