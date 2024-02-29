95db69eb21787ed5fd76c358afc95a7098cff991
Sanmollers <mollersjean0@gmail.com>
2024-02-29 12:42:08 +0100

    TP2 : création Gaulois[] villageois

--- src/village/Village.java
+++ src/village/Village.java
@@ -1,14 +1,25 @@
 package village;
 
 import personnages.Chef;
+import personnages.Gaulois;
 
 public class Village {
 	private String nom;
 	private Chef chef;
+	private Gaulois[] villageois;
+	private int nbVillageois = 0;
+	private int nbVillageoisMaximum;
 
 	public Village(String nom, Chef chef) {
 		this.nom = nom;
 		this.chef = chef;
+		this.villageois.length() = nbVillageois ;
+		villageois = new Gaulois[nbVillageoisMaximum];
+		
+	}
+	public void ajouterHabitant(Gaulois gaulois, Gaulois[] villageois) {
+		// TODO Auto-generated method stub
+
 	}
 
 	public String getNom() {
