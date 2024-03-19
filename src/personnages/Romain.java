cf18a4e1cb87bb0c0d0cbcaef2d4947c290347d5
Sanmollers <mollersjean0@gmail.com>
2024-03-19 12:58:09 +0100

    a effacé

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -22,16 +22,19 @@ public class Romain {
 	}
 
 	public void recevoirCoup(int forceCoup) {
+		assert force>0;
+		int force1 = force;
 		force -= forceCoup;
 		if (force > 0) {
 			parler("Aie");
 		} else {
 			parler("J'abandonne...");
 		}
+		assert force1>force;
 	}
 
 	public static void main(String[] args) {
-		Romain romus = new Romain("Romus", 6);
+		Romain romus = new Romain("Romus", -6);
 		System.out.println(romus.prendreParole());
 		romus.parler("C'est Romus qui parle !");
 		romus.recevoirCoup(2);
